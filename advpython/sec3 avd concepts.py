b9ea5f610ce10d8c954a8fc79210c6d6e0f52ee4
neeladalja14 <nadal001@plattsburgh.edu>
2020-07-02 02:05:58 -0400

    this commit has how the classes in python is used and how it works.also there is an example of how the BREAK and CONTINUE works in phton in loops.

---
+++ advpython/sec3 avd concepts.py
@@ -0,0 +1,49 @@
+import random
+
+
+class Enemy:
+
+    hp = 200
+
+    def __init__(self, atkl, atkh):
+        self.atkl = atkl
+        self.atkh = atkh
+
+    def getAtk(self):
+        print("atk is ", self.atkl)
+
+    def getHp(self):
+        print("HP is ", self.hp)
+
+enemy1 = Enemy(40, 49)
+enemy1.getAtk()
+enemy1.getHp()
+
+enemy2 = Enemy(75, 90)
+enemy2.getAtk()
+enemy2.getHp()
+
+
+
+"""
+playerhp = 260
+enemyatkl = 60
+enemyatkh = 80
+
+
+while playerhp > 0:
+    dmg = random.randrange(enemyatkl, enemyatkh)
+    playerhp = playerhp - dmg
+
+    if playerhp <= 30:
+        playerhp = 30
+
+    print("enemy strikes for ", dmg, "points of damage. current HP is: ", playerhp)
+
+    if playerhp > 30:
+        continue
+
+    print("you have been teleported to nearest destination for respawn.")
+    break
+
+"""
