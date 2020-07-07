16704881a2bc3f81b55a12bab8fb963968fdc158
neeladalja14 <nadal001@plattsburgh.edu>
2020-07-07 16:10:15 -0400

    this commit has some features in the game to heal, and attack, also choose they type of attack and type if heal.

--- advpython/battle/main.py
+++ advpython/battle/main.py
@@ -1,5 +1,7 @@
 from classes.game import Person, bcolors
 from classes.magic import Spell
+from classes.inventory import Item
+
 
 # Create Black Magic
 fire = Spell("Fire", 10, 100, "black")
@@ -12,9 +14,22 @@ quake = Spell("Quake", 10, 100, "black")
 cure = Spell("Cure", 12, 120, "white")
 cura = Spell("Cura", 18, 200, "white")
 
+
+# Create Some Item
+potion = Item("Potion", "potion", "Heals 50 HP", 50)
+hipotion = Item("Hi-potion", "potion", "potion Heals 100 HP", 100)
+superpotion = Item("Super Potion", "potion", "potion Heals 500 HP", 500)
+elixer = Item("Elixer", "elixer", "Fully restores HP/MP pf one party member" , 9999)
+hielixer = Item("Megaelixer", "elixer", "Fully restores party's HP/MP", 9999)
+
+grenade = Item("Grenade", "attack", "deals 500 degree", 500)
+
+player_spell = [fire, thunder, blizzard, meteor, quake, cure, cura]
+player_items = [potion, hipotion, superpotion, elixer, hielixer, grenade]
+
 # Instantiate People
-player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, quake, cure, cura])
-enemy = Person(1200, 65, 45, 25, [])
+player = Person(460, 65, 60, 34, player_spell, player_items)
+enemy = Person(1200, 65, 45, 25, [], [])
 
 running = True
 i = 0
@@ -36,6 +51,8 @@ while running:
         player.choose_magic()
         magic_choice = int(input("Choose Magic: ")) - 1
 
+        if magic_choice == -1:
+            continue
 
         spell = player.magic[magic_choice]
         magic_dmg = spell.generate_damage()
@@ -56,6 +73,19 @@ while running:
         elif spell.type == "black":
             enemy.take_damage(magic_dmg)
             print(bcolors.OKBLUE + "\n" + spell.name + "deals", str(magic_dmg), "points of damage" )
+
+    elif index == 2:
+        player.choose_item()
+        item_choice = int(input("Choose item")) - 1
+
+        if item_choice == -1:
+            continue
+
+        item = player.items[item_choice]
+
+        if item.type == "potion":
+            player.heal(item.prop)
+            print(bcolors.OKGREEN + "\n" + " heals for", str(item.prop), "HP" + bcolors.ENDC)
 
     enemy_choice = 1
 
