c96f430d2580bbba1ee7d272d3b96ba70953a799
neeladalja14 <nadal001@plattsburgh.edu>
2020-07-10 04:27:36 -0400

    this commit has code that shows how to post something to a url like online forms.

--- advpython/chfiles/requesttest.py
+++ advpython/chfiles/requesttest.py
@@ -1,32 +1,7 @@
 import requests
-from io import BytesIO
-from PIL import Image
 
-r = requests.get("https://wallpapercave.com/wp/TuVhQdr.jpg")
+my_data = {"name": "Nick", "email": "nicj@exampple.co"}
+r = requests.post("http://www.w3schools.com/php/welcome.php", data=my_data)
 
-print("Status code: ", r.status_code)
-
-image = Image.open(BytesIO(r.content))
-
-print(image.size, image.format, image.mode)
-path = "./image" + image.format
-
-try:
-    image.save(path, image.format)
-except IOError:
-    print("Cannot save images.")
-
-
-
-
-
-"""params = {"q": "pizza"}
-
-r = requests.get("http://www.bing.com/search", params=params)
-
-print("Status: ", r.status_code)
-
-print(r.url)
-
-f = open("./page.html", "w+")
-f.write(r.text)"""
+f = open("myfile.html", "w+")
+f.write(r.text)
